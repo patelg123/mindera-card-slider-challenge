4894b5561cfe0958f358f2b5fe7ebfee28925913
Girish Patel <patelg123@gmail.com>
2018-11-28 14:46:22 +0000

    Added Left and Right Slider functionality

--- src/components/CardSlider/CardSlider.js
+++ src/components/CardSlider/CardSlider.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import CardContainer from '../CardContainer/CardContainer';
 import './CardSlider.css';
+import green_arrow_left from './images/green_arrow_left.png';
+import green_arrow_right from './images/green_arrow_right.png';
+
+const restURL = 'http://localhost:3001/cards?';
 
 class CardSlider extends Component {
   constructor(props) {
@@ -8,12 +12,14 @@ class CardSlider extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      data: []
+      data: [],
+      NumberOfCards: 3,
+      CurrentCardStart: 0,
+      CurrentCardEnd: 3
     };
   }
 
-  loadData() {
-    let restURL = "http://localhost:3001/cards?_start=0&_end=3";
+  loadData(restURL) {
 
     fetch(restURL)
       .then(res => res.json())
@@ -33,8 +39,44 @@ class CardSlider extends Component {
       )
     }
 
+  handleRightClick = () => {
+
+    let cardStart = this.state.CurrentCardEnd;
+    let cardEnd = this.state.NumberOfCards + this.state.CurrentCardEnd;
+
+    this.loadData(restURL + '_start=' + cardStart + '&_end=' + cardEnd);
+
+    this.setState({
+      CurrentCardStart: cardStart,
+      CurrentCardEnd: cardEnd
+    });
+
+  }
+
+  handleLeftClick = () => {
+
+    let cardStart = this.state.CurrentCardStart - this.state.NumberOfCards;
+    let cardEnd = this.state.CurrentCardStart;
+
+    this.loadData(restURL + '_start=' + cardStart + '&_end=' + cardEnd);
+
+    this.setState({
+      CurrentCardStart: cardStart,
+      CurrentCardEnd: cardEnd
+    });
+
+  }
+
   componentDidMount() {
-  		this.loadData()
+
+      let cardStart = this.state.CurrentCardStart;
+      let cardEnd = this.state.NumberOfCards;
+
+      this.loadData(restURL + '_start=' + cardStart + '&_end=' + cardEnd);
+
+      this.setState({
+        CurrentCardEnd: cardEnd
+      });
   }
 
   render() {
@@ -50,7 +92,15 @@ class CardSlider extends Component {
        return (
          <div className="container">
           <CardContainer data={data} />
+
+          <div>
+            <img src={green_arrow_left}  onClick={() => this.handleLeftClick()} />
+            <img src={green_arrow_right} onClick={() => this.handleRightClick()} />
           </div>
+
+         </div>
+
+
        );
      }
   }
